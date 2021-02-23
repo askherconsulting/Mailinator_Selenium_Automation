a25c56e4090d8d1cf339c0fafd69c5f463ddffb3
elizabeth.marshall <elizabeth.marshall@smoothwall.com>
2021-02-23 21:39:01 +0000

    add new methods to driver class

--- Framework/Selenium/Driver.cs
+++ Framework/Selenium/Driver.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using OpenQA.Selenium;
@@ -27,6 +28,16 @@ namespace Framework.Selenium
             Debug.WriteLine(url);
             Current.Navigate().GoToUrl(url);
         }
+
+        public static IWebElement FindElement(By by)
+            {
+                return Current.FindElement(by);
+            }
+
+        public static IList<IWebElement> FindElements(By by)
+            {
+                return Current.FindElements(by);
+            }
 
 
         public static IWebDriver Current => _driver ?? throw new NullReferenceException("_driver is null");
